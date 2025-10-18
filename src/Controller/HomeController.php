4314ce9400e4d69afb5700e49c24191069015c0f
AngeloCinaWD <angelo.cina@yahoo.it>
2025-10-18 14:07:03 +0200

    attribute #[Route]

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Routing\Attribute\Route;
+
 class HomeController
 {
+    #[Route('/')]
     public function index()
     {
         echo "Hello from a controller!!";
