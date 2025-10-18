61b36f0712158a8081ce28e3f27a8b32afb9ed0a
AngeloCinaWD <angelo.cina@yahoo.it>
2025-10-18 14:23:19 +0200

    installed bundle composer require symfony/twig-bundle

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SYMFONY PROJECT: php 8.3.26
 
 ## [thksTo](https://www.youtube.com/watch?v=i_jgWZItCGI&list=PLFbnPuoQkKsc5yjrpKZz9Zz9SBtoXHPlF)
+
+## [Templates](https://symfony.com/doc/current/templates.html#installation)
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,21 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
     #[Route('/')]
     public function index(): Response
     {
 //        echo "Hello from a controller!!";
-        return new Response("Hello from a controller!!");
+
+        // la path qui inserita è relativa alla cartella templates
+        $contents = $this->renderView('home/index.html.twig');
+
+//        return new Response("Hello from a controller!!");
+        return new Response($contents);
     }
 }
